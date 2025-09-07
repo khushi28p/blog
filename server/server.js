4d4dff79c81d033f5f0efab255ea3857d3c9b796
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-09-07 15:05:38 +0530

    changes directory structure

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import 'dotenv/config'
-import cors from 'cors'
+import "dotenv/config";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import { adminRouter } from "./routes/adminRoutes.js";
 import { blogRouter } from "./routes/blogRoutes.js";
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!!!");
-})
+  res.send("Hello World!!!");
+});
 
 app.use("/api/admin", adminRouter);
 app.use("/api/blog", blogRouter);
@@ -22,5 +22,5 @@ app.use("/api/blog", blogRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
